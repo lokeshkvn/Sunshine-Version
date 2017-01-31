b7f0610952006e30cbe8f3ab0cd3193aa3b67356
LOKESH NANDANWAR <lokeshkvn321998@gmail.com>
2017-01-31 20:23:28 +0530

    Added Test Cases and SQLITE Database helper class

---
+++ app/src/androidTest/java/com/example/android/sunshine/app/FullTestSuite.java
@@ -0,0 +1,21 @@
+package com.example.android.sunshine.app;
+
+/**
+ * Created by DELL PC on 31-01-2017.
+ */
+
+import android.test.suitebuilder.TestSuiteBuilder;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+public class FullTestSuite extends TestSuite {
+    public static Test suite() {
+        return new TestSuiteBuilder(FullTestSuite.class)
+                .includeAllPackagesUnderHere().build();
+    }
+
+    public FullTestSuite() {
+        super();
+    }
+}
